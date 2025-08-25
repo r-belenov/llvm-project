171df16e1d83bbe6753d0d5c38f8193cf1685e20
Roman Belenov <103195329+r-belenov@users.noreply.github.com>
2025-08-25 14:13:53 +0300

    Whitespace removed

--- llvm/tools/llvm-mca/llvm-mca.cpp
+++ llvm/tools/llvm-mca/llvm-mca.cpp
@@ -634,7 +634,7 @@ int main(int argc, char **argv) {
       const SmallVector<mca::Instrument *> Instruments =
           InstrumentRegions.getActiveInstruments(Loc);
 
-      auto Latency = Region->getExplicitLatency(Index);      
+      auto Latency = Region->getExplicitLatency(Index);
       Expected<std::unique_ptr<mca::Instruction>> Inst = Latency ?
           IB.createInstruction(MCI, Instruments, [=](llvm::mca::InstrDesc& ID) {
             for (auto& W : ID.Writes) W.Latency = *Latency;
