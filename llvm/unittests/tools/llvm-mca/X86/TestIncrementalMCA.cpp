bc775d4415a825a4fe5bbee0f2d67d9290e1cfbc
Roman Belenov <103195329+r-belenov@users.noreply.github.com>
2025-08-26 14:11:55 +0300

    Added missing result object

--- llvm/unittests/tools/llvm-mca/X86/TestIncrementalMCA.cpp
+++ llvm/unittests/tools/llvm-mca/X86/TestIncrementalMCA.cpp
@@ -242,5 +242,6 @@ TEST_F(X86TestBase, TestInstructionCustomization) {
   // Run the baseline.
   json::Object BaselineResult;
   auto E = runBaselineMCA(BaselineResult, MCIs);
-  auto V = ResultObj->getInteger("TotalCycles");
+  auto *BaselineObj = BaselineResult.getObject(SV->getNameAsString());
+  auto V = BaselineObj->getInteger("TotalCycles");
 }
