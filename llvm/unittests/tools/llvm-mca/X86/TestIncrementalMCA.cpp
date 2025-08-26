d488097ea1805dbddbeeba43da88b150bf25e008
Roman Belenov <103195329+r-belenov@users.noreply.github.com>
2025-08-26 16:24:07 +0300

    Adding missing const

--- llvm/unittests/tools/llvm-mca/X86/TestIncrementalMCA.cpp
+++ llvm/unittests/tools/llvm-mca/X86/TestIncrementalMCA.cpp
@@ -247,7 +247,7 @@ TEST_F(X86TestBase, TestInstructionCustomization) {
   // Run the baseline.
   json::Object BaselineResult;
   auto E = runBaselineMCA(BaselineResult, MCIs, {}, nullptr,
-      [=](InstrBuilder& IB, MCInst& MCI, const SmallVector<Instrument*>& Instruments) {
+      [=](InstrBuilder& IB, const MCInst& MCI, const SmallVector<Instrument*>& Instruments) {
         return IB.createInstruction(IB, MCI, Instruments,
           [=](InstrDesc& ID) {
             for (auto& W : ID.Writes) W.Latency = ExplicitLatency;
