b86ae7e8b37a5cbf9c0ee76e1b696c0635d4fc74
Roman Belenov <103195329+r-belenov@users.noreply.github.com>
2025-08-26 09:41:24 +0300

    Parse latency comment

--- llvm/tools/llvm-mca/CodeRegionGenerator.cpp
+++ llvm/tools/llvm-mca/CodeRegionGenerator.cpp
@@ -95,6 +95,18 @@ void AnalysisRegionCommentConsumer::HandleComment(SMLoc Loc,
     return;
 
   Comment = Comment.drop_front(Position);
+  if (Comment.starts_with("LLVM-MCA-LATENCY")) {
+      auto Parts = Comment.split(':');
+      Position = Comment.find_first_not_of(" \t");
+      if (Position >= Parts.second.size())
+        return;
+      Parts.second.drop_front(Position);
+      unsigned Latency = 0;
+      if (!Parts.second.getAsInteger(10, Latency))
+        Streamer.AddLatencyAnnotation(Latency);
+      return;
+  }
+
   if (Comment.consume_front("LLVM-MCA-END")) {
     // Skip spaces and tabs.
     Position = Comment.find_first_not_of(" \t");
